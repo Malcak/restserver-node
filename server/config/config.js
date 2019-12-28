cf23b5ad07e77aabc0cdf395d53cbaa0ffb59bcf
Malcak <Malcakx@gmail.com>
2019-12-27 20:37:07 -0500

    rest-server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,19 @@
 // =======================
 // Puerto
 // =======================
-process.env.Port = process.env.Port || 3000
+process.env.Port = process.env.Port || 3000;
+// =======================
+// Entorno
+// =======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+// =======================
+// Base de datos
+// =======================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:UI7LF8uRyMATNSe5@cluster0-pcg4w.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
